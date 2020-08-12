d0590c093fec7862b29dcec97fe000c2bf7b5535
STEVEN EDWARDS <stevenge@email.sc.edu>
2020-08-11 21:30:53 -0400

    initial push

---
+++ Connector.java
@@ -0,0 +1,17 @@
+/*import java.sql.*;
+public class Connector {
+   public static Connection connect(){
+       Connection link = null;
+       
+       try{
+        Class.forName("com.mysql.jdbc.Driver");
+        System.out.println("Connecting to database...");
+        link = DriverManager.getConnection(url);
+       }
+       catch(ClassNotFoundException e){
+           System.out.println(e+"");
+       }
+
+       return link;
+   }
+}*/
